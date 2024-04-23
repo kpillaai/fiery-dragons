f577ac3eb206435e810455781b48a9a96bbb03c1
zche0160 <chen.zilei@hotmail.com>
2024-04-23 18:44:58 +1000

    Created scenes for the start of the game and buttons to change scenes

--- src/main/java/org/openjfx/fierydragons/render/BoardController.java
+++ src/main/java/org/openjfx/fierydragons/render/BoardController.java
@@ -4,15 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.openjfx.fierydragons.StartApplication;
 
-
 import java.io.IOException;
 
-public class SceneController {
+public class BoardController {
 
-    public void switchToSettingScene(ActionEvent event) throws IOException {
+    public void switchToSettingScene(MouseEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("game-settings.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load());
@@ -20,11 +20,7 @@ public class SceneController {
         stage.show();
     }
 
-    public void switchToBoardScene(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("game-board.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+    public void onCardClick(MouseEvent event) {
+        System.out.printf("circle clicked");
     }
 }
